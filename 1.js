e72433b32c5b05dbaa5ab5fe1140a924a0a39653
afflz <augustovirtual317@gmail.com>
2016-04-23 11:40:54 -0300

    Ex_JS_prontos

---
+++ 1.js
@@ -0,0 +1,9 @@
+max = prompt("digite um valor");
+
+for (j = 1;j<=max;j++){
+var str = "";
+  for (i = 0;i<j;i++){
+    str += "#";
+  }
+    console.log(str);
+}
---
+++ 3.js
@@ -0,0 +1,6 @@
+string = prompt("digite um valor");
+
+if (string === string.split("").reverse().join(""))
+  console.log("É um palindromo");
+else
+  console.log("Não é um palindromo");
